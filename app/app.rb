daed3b1a24e9d2eed24951a4911ceb60c47f0014
Charlie Person <charlie.person@hyperisland.se>
2015-12-16 10:44:10 +0000

    CH10 completed

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,14 @@
 require 'sinatra/base'
+require './lib/link.rb'
 
 class Bookmark < Sinatra::Base
   get '/' do
     erb(:index)
+  end
+
+  get '/links' do
+    @links = Link.all
+    erb(:links)
   end
 
   # start the server if ruby file executed directly
